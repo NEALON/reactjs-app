11edadb9295071408572a670ca8fabd36b2c91ae
Eugene Tsurcan <eugene.tsurcan@gmail.com>
2017-10-25 22:20:18 +0300

    changed Readme.md

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+// import "./styles/App.css";
+// import { Button } from 'react-bootstrap';
+// import logo from './logo.svg';
+
+const App = () => {
+  return <div>Hello World</div>
+}
+
+export default App;
